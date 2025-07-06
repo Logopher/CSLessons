918e290753379b42c5851dc46e2f0b7255d7f5ac
Grault <427389+Logopher@users.noreply.github.com>
2025-07-05 21:10:35 -0500

    Added documentation links for Console API methods.

--- Lessons/Chapter1/Lesson09.cs
+++ Lessons/Chapter1/Lesson09.cs
@@ -17,9 +17,12 @@ namespace Lessons.Chapter1
                 Console.WriteLine("Random number: " + random.Next(1, 101));
             }
 
+            // https://learn.microsoft.com/en-us/dotnet/api/system.console.readline?view=net-8.0
             Console.ReadLine();
 
             Console.WriteLine("Press any key to exit...");
+
+            // https://learn.microsoft.com/en-us/dotnet/api/system.console.readkey?view=net-8.0
             Console.ReadKey(true);
 
             // Use ideas presented so far to build a game where the user reapeatedly guesses at a random number.
